20b62368b10a9b5975f90b4330d0551448f4ff8b
anisha-shamsudeen <anisha.shamsudeen@edu.esiee.fr>
2025-11-27 09:13:07 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #### Fonctions secondaires
 
+"""Module qui retourne les valeurs de la suite de syracuse pour une source n,
+le temps de vol, l'altitude maximale et le temps de vol en altitude de cette suite"""
 
 # imports
 from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """Création du graphique
+    
+    Args :
+        lsyr : le tableau contenant les valeurs de la suite de syracuse
+
+    Returns : 
+        None
+    """
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -32,8 +42,14 @@ def syracuse_l(n):
         list: la suite de Syracuse de source n
     """
 
-    # votre code ici 
-    l = [ ]
+    # votre code ici
+    l = [n]
+    while n != 1 :
+        if n % 2 == 0 :
+            n = n/2
+        else :
+            n = n*3 + 1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +61,15 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
+
     # votre code ici
 
-    n = 0
-    return n
+    for n in l :
+        if n == 1 :
+            return l.index(n)
+    return None
+
+
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -63,8 +83,10 @@ def temps_de_vol_en_altitude(l):
 
     # votre code ici
 
-    n = 0
-    return n
+    for i in range (0,len(l) - 1) :
+        if l[i+1] < l[0] :
+            return i
+    return None
 
 
 def altitude_maximale(l):
@@ -76,18 +98,22 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
+
     # votre code ici
-    
-    n = 0
-    return n
+
+    am = l[0]
+    for n in l[1:len(l)] :
+        am = max(am,n)
+        #if n > am :
+        #    am = n
+    return am
 
 
 #### Fonction principale
 
 
 def main():
-
+    """ Exécution des fonctions secondaires"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
